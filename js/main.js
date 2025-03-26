8d436a1f66358716096ffc84d1e0048a0a807675
MariaProgrammer <maryfrontend22@gmail.com>
2025-03-26 17:34:36 +0300

    contacts mob button added

--- js/main.js
+++ js/main.js
@@ -358,9 +358,39 @@ burgerButton.addEventListener('click', function() {
 })
 const footerBurgerButton = document.querySelector('.footer-burger__button')
 const footerBurgerMenu = document.querySelector('.footer-burger__menu')
+
 footerBurgerButton.addEventListener('click', function() {
+  
   footerBurgerMenu.classList.toggle('active')
 })
+
+//табы
+document.addEventListener("DOMContentLoaded", () => {
+  const tabs = document.querySelector('.tabs')
+  const tabsBtn =document.querySelectorAll('.tabs__btn')
+  const tabsContent =document.querySelectorAll('.tabs__content')
+  
+  if(tabs) {
+    tabs.addEventListener('click', (e) => {
+      if(e.target.classList.contains('tabs__btn')) {
+        const tabsPath = e.target.dataset.tabsPath;
+        tabsHandler(tabsPath)
+      }
+
+    })
+    
+  };
+  const tabsHandler = (path) => {
+    tabsBtn.forEach(item => item.classList.remove('tabs__btn-active'))
+    document.querySelector(`[data-tabs-path="${path}"]`).classList.add('tabs__btn-active')
+    tabsContent.forEach(item => item.classList.remove('tabs__content-active'))
+    document.querySelector(`[data-tabs-target="${path}"]`).classList.add('tabs__content-active')
+
+  }
+  
+  })
+  
+
 
 
   
